d82b34f6f9d9abf8fa581baf40c86eacded50658
manivannanpannerselvam <manipinnai238@gmail.com>
2025-11-16 22:19:31 +0800

    Added product details

--- features/pages/product_page.py
+++ features/pages/product_page.py
@@ -1,15 +1,24 @@
-# from .base_page import Base_Page
-# from playwright.sync_api import Page
+from .base_page import Base_Page
+from playwright.sync_api import Page
 
-# class Product_Page(Base_Page):
+class Product_Page(Base_Page):
 
-#     locators = {
-#         "samsung_icon": "Samsung galaxy s6"
+    locators = {
+        "samsung_icon": "Samsung galaxy s6",
+        "city": "#city",
+        "credit_card": "#card",
+        "month": "#month",
+        "year": "#year",
+        "Purchase": "Purchase"
      
-#     }
+    }
     
-#     def __init__(self, page: Page):
-#         super().__init__(page)
+    def __init__(self, page: Page):
+        super().__init__(page)
 
-#     def clickproduct(self):
-#         self.page.get_by_role("link").filter(has_text=self.locators["samsung_icon"]).click()
+    def clickproduct(self):
+        self.page.get_by_role("link").filter(has_text=self.locators["samsung_icon"]).click()
+
+    def clickpurchasebutton(self):
+        self.page.wait_for_timeout(1000)
+        self.page.get_by_role("button", name=self.locators["Purchase"]).click()
